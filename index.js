ee13440a50502177a73f7ed8e1906281cae8fb85
Avishek Mishra <131962523+Avishek3223@users.noreply.github.com>
2024-09-19 09:29:31 +0530

    Update index.js
    port change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-const port = process.env.PORT || 3000;
+const port = 5000;
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log('MongoDB connected'))
